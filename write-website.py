8904ebb9663feac71f720d836a3ee3ab34ed2984
Alex Jurkiewicz <alex@jurkiewi.cz>
2016-04-13 23:45:00 +1000

    First pass at player stats page.

--- write-website.py
+++ write-website.py
@@ -2,13 +2,40 @@
 
 import os
 import json
+import datetime
 
 import jinja2
 
 OUTDIR = 'dcss-scoreboard-html'
 
+def prettyint(value):
+    """Jinja filter to prettify ints.
+
+    eg, 1234567 to '1 234 567'.
+    """
+    return "{0:,}".format(int(value))
+
+def prettydur(duration):
+    """Jinja filter to convert seconds to a pretty duration.
+
+    eg, 170 to '2 minutes, 50 seconds'.
+    """
+    return str(datetime.timedelta(seconds=int(duration)))
+
+def prettycounter(counter, reverse=True):
+    """Jinja filter to convert a counter dict to pretty text.
+
+    Set reverse=False to sort ascending instead of descending.
+
+    eg, {'a':1, 'b': 3, 'c': 2} to 'a (3), c (2), b (1)'.
+    """
+    return ", ".join("{k} ({v})".format(k=k, v=v) for k, v in sorted(counter.items(), key=lambda i: i[1], reverse=reverse))
+
 if __name__ == '__main__':
     env = jinja2.Environment(loader=jinja2.FileSystemLoader('html_templates'))
+    env.filters['prettyint'] = prettyint
+    env.filters['prettydur'] = prettydur
+    env.filters['prettycounter'] = prettycounter
 
     data = json.loads(open('scoring_data.json').read())
 
@@ -16,14 +43,17 @@ if __name__ == '__main__':
     if not os.path.isdir(OUTDIR):
         os.mkdir(OUTDIR)
 
+    print("Writing index")
     with open(os.path.join(OUTDIR, 'index.html'), 'w') as f:
         template = env.get_template('index.html')
         f.write(template.render())
 
+    print("Writing highscores")
     with open(os.path.join(OUTDIR, 'highscores.html'), 'w') as f:
         template = env.get_template('highscores.html')
         f.write(template.render(highscores=data['global_stats']))
 
+    print("Writing players")
     player_html_path = os.path.join(OUTDIR, 'players')
     if not os.path.isdir(player_html_path):
         os.mkdir(player_html_path)
@@ -33,6 +63,7 @@ if __name__ == '__main__':
         f.write(template.render(players=players))
 
     for player, stats in data['players'].items():
+        print("Writing player %s" % player)
         outfile = os.path.join(player_html_path, player + '.html')
         with open(outfile, 'w') as f:
             template = env.get_template('player.html')
