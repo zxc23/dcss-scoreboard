27419205e55ad4b6bb53e88205e965f9fc0dc411
Alex Jurkiewicz <alex@jurkiewi.cz>
2016-04-14 10:57:21 +1000

    Use Jinja2 for HTML templates.

--- write-website.py
+++ write-website.py
@@ -5,37 +5,34 @@ import sys
 import json
 import pprint
 
+import jinja2
+
 OUTDIR = 'dcss-scoreboard-html'
 
 if __name__ == '__main__':
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader('html_templates'))
+
     data = json.loads(open('scoring_data.json').read())
 
     print("Writing HTML to %s" % OUTDIR)
     os.makedirs(OUTDIR)
 
-    with open(os.path.join(OUTDIR, 'index.htm'), 'w') as f:
-        f.write("<h1>Homepage</h1>\n")
-        f.write("""<p><a href="highscores.htm">Global Highscores</a></p>\n""")
-        f.write("""<p><a href="players.htm">Player Pages</a></p>\n""")
+    with open(os.path.join(OUTDIR, 'index.html'), 'w') as f:
+        template = env.get_template('index.html')
+        f.write(template.render())
 
-    with open(os.path.join(OUTDIR, 'highscores.htm'), 'w') as f:
-            f.write("<pre>\n")
-            pprint.pprint(data['global_stats'], f)
-            f.write("</pre>\n")
+    with open(os.path.join(OUTDIR, 'highscores.html'), 'w') as f:
+        template = env.get_template('highscores.html')
+        f.write(template.render(highscores=data['global_stats']))
 
     player_html_path = os.path.join(OUTDIR, 'players')
     os.makedirs(player_html_path)
-    with open(os.path.join(OUTDIR, 'players.htm'), 'w') as f:
-        f.write("<h1>All players</h1>\n")
-        f.write("<ul>\n")
-        for player in data['players'].keys():
-            f.write("""<li><a href="players/{player}.htm">{player}</a></li>\n""".format(player=player))
-        f.write("</ul>\n")
+    with open(os.path.join(OUTDIR, 'players.html'), 'w') as f:
+        template = env.get_template('players.html')
+        f.write(template.render(players=data['players'].keys()))
 
     for player, stats in data['players'].items():
-        outfile = os.path.join(player_html_path, player + '.htm')
+        outfile = os.path.join(player_html_path, player + '.html')
         with open(outfile, 'w') as f:
-            f.write("<h1>%s's stats<br>\n" % player)
-            f.write("<pre>\n")
-            pprint.pprint(stats, f)
-            f.write("</pre>\n")
+            template = env.get_template('player.html')
+            f.write(template.render(player=player, stats=stats))
