e03d4d65800f86c1f32b4760a6d25a1f2dcbbead
Alex Jurkiewicz <alex@jurkiewi.cz>
2016-04-16 13:45:20 +1000

    Finish database loader implementation.
    Still a bit hacky -- we store the game as a single text column so you
    can't search the database for games matching certain conditions. But
    that can come later.

--- dcss_scoreboard/log_import.py
+++ dcss_scoreboard/log_import.py
@@ -5,31 +5,88 @@ import os
 import sys
 import re
 
+import sqlalchemy
 from sqlalchemy import create_engine
-engine = create_engine('sqlite:///scoredata.db', echo=True)
+engine = create_engine('sqlite:///scoredata.db', echo=False)
+
+
+def sqlite_performance_over_safety(dbapi_con, con_record):
+    """Significantly speeds up inserts but will break on crash."""
+    dbapi_con.execute('PRAGMA journal_mode = MEMORY')
+    dbapi_con.execute('PRAGMA synchronous = OFF')
+
+
+sqlalchemy.event.listen(engine, 'connect', sqlite_performance_over_safety)
 
 model.metadata.create_all(engine)
 
 conn = engine.connect()
 
+
 def add_game(gid, data):
-    conn.execute(model.game.insert(), gid=gid, logfile=data)
+    """Add a game to the database."""
+    try:
+        conn.execute(model.game.insert(), gid=gid, logfile=data)
+    except sqlalchemy.exc.IntegrityError:
+        print("Duplicate game, ignoring:", gid)
+
+
+def get_log_pos(logfile):
+    """Get the number of lines we've already processed."""
+    pass
+    # print(dir(model.log_progress.c))
+    s = model.log_progress.select().where(model.log_progress.c.logfile ==
+                                          logfile)
+    row = conn.execute(s).fetchone()
+    if row:
+        return row.lines_parsed
+    else:
+        return 0
+
+
+def save_log_pos(logfile, pos):
+    """Save the number of lines we've processed."""
+    # print("Saving log pos for", logfile, "as", pos)
+    try:
+        conn.execute(model.log_progress.insert(),
+                     logfile=logfile,
+                     lines_parsed=pos)
+    except sqlalchemy.exc.IntegrityError:
+        conn.execute(model.log_progress.update().where(
+            model.log_progress.c.logfile == logfile).values(lines_parsed=pos))
+
 
 def calculate_game_gid(log):
-    # XXX: should include server source
+    """Calculate GID for a game. Sequell compatible.
+
+    XXX: should include server source.
+    """
     return "%s:%s" % (log['name'], log['start'])
 
+
 def load_logfiles():
+    """Read logfiles and parse their data.
+
+    XXX: should be given a filename to read, not figure it out ourselves.
+    """
     import glob
-    pat = '(?<!:):(?!:)'  # logfile format escapes : as ::, so we need to split with re.split instead of naive line.split(':')
+    # logfile format escapes : as ::, so we need to split with re.split
+    # instead of naive line.split(':')
+    pat = '(?<!:):(?!:)'
     for logfile in glob.glob("logfiles/*"):
         if os.stat(logfile).st_size == 0:
             continue
-        print("Reading %s... " % logfile, end='')
         sys.stdout.flush()
         lines = 0
+        processed_lines = get_log_pos(logfile)
+        print("Reading %s%s... " % (logfile,
+                                    (" from line %s" % processed_lines) if
+                                    processed_lines else ''),
+              end='')
         for line in open(logfile).readlines():
             lines += 1
+            if lines <= processed_lines:
+                continue
             if not line:  # skip blank lines
                 continue
             log = {}
@@ -40,8 +97,9 @@ def load_logfiles():
                 if v.isdigit():
                     v = int(v)
                 else:
-                    v = v.replace("::", ":") # Logfile escaping as per above
+                    v = v.replace("::", ":")  # Logfile escaping as per above
                 log[k] = v
             gid = calculate_game_gid(log)
             add_game(gid, log)
-        print("done (%s lines)" % lines)
+        print("done (%s new lines)" % (lines - processed_lines))
+        save_log_pos(logfile, lines)
--- dcss_scoreboard/model.py
+++ dcss_scoreboard/model.py
@@ -1,25 +1,33 @@
 """Defines the database models for this module."""
 
 from sqlalchemy import TypeDecorator, MetaData, Table, Column
-from sqlalchemy import String
+from sqlalchemy import String, Integer
 import json
 import sqlalchemy.ext.mutable
 
+
 class JsonEncodedDict(TypeDecorator):
-  """Enables JSON storage by encoding and decoding on the fly."""
-  impl = String
+    """Enables JSON storage by encoding and decoding on the fly."""
 
-  def process_bind_param(self, value, dialect):
-    return json.dumps(value)
+    impl = String
 
-  def process_result_value(self, value, dialect):
-    return json.loads(value)
+    def process_bind_param(self, value, dialect):
+        """Dict -> JSON String."""
+        return json.dumps(value)
+
+    def process_result_value(self, value, dialect):
+        """JSON String -> Dict."""
+        return json.loads(value)
 
 sqlalchemy.ext.mutable.MutableDict.associate_with(JsonEncodedDict)
 
 metadata = MetaData()
 
 game = Table('games', metadata,
-    Column('gid', String, primary_key=True),
-    Column('logfile', JsonEncodedDict),
+             Column('gid', String, primary_key=True),
+             Column('logfile', JsonEncodedDict),
 )
+
+log_progress = Table('log_progress', metadata,
+                     Column('logfile', String, primary_key=True),
+                     Column('lines_parsed', Integer, nullable=False))
