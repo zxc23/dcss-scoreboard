911f4a01ac5df8e7b67234c3fb11ab86e6bd39e6
Alex Jurkiewicz <alex@jurkiewi.cz>
2016-04-17 19:45:36 +1000

    dcss_scoreboard -> scoreboard. Simpler names FTW.

--- loader.py
+++ loader.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-import dcss_scoreboard.log_import
-import dcss_scoreboard.scoring
-import dcss_scoreboard.write_website
+import scoreboard.log_import
+import scoreboard.scoring
+import scoreboard.write_website
 
 dcss_scoreboard.log_import.load_logfiles()
 dcss_scoreboard.scoring.score_games()
