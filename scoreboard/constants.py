4c38201ded1adcfb764ea943ddf0a832072b2fde
David Kerksal <dkerksal@gmail.com>
2016-04-18 02:05:48 +1000

    Add constants file

---
+++ scoreboard/constants.py
@@ -0,0 +1,31 @@
+"""Defines useful constants."""
+
+PLAYABLE_RACES = {'Ce', 'DD', 'DE', 'Dg', 'Ds', 'Dr', 'Fe', 'Fo', 'Gh', 'Gr',
+                  'HE', 'HO', 'Ha', 'Hu', 'Ko', 'Mf', 'Mi', 'Mu', 'Na', 'Op', 'Og', 'Sp', 'Te',
+                  'Tr', 'VS', 'Vp'}
+PLAYABLE_ROLES = {'AE', 'AK', 'AM', 'Ar', 'As', 'Be', 'CK', 'Cj', 'EE', 'En',
+                  'FE', 'Fi', 'Gl', 'Hu', 'IE', 'Mo', 'Ne', 'Sk', 'Su', 'Tm', 'VM', 'Wn', 'Wr',
+                  'Wz'}
+PLAYABLE_GODS = {'Ashenzari', 'Beogh', 'Cheibriados', 'Dithmenos', 'Elyvilon',
+                 'Fedhas', 'Gozag', 'Jiyva', 'Kikubaaqudgha', 'Lugonu', 'Makhleb', 'Nemelex Xobeh',
+                 'Okawaru', 'Pakellas', 'Qazlal', 'Ru', 'Sif Muna', 'the Shining One', 'Trog',
+                 'Vehumet', 'Xom', 'Yredelemnul', 'Zin'}
+RACE_TO_GREAT_RACE = {'Ce': 'greatcentaur', 'DD': 'greatdeepdwarf',
+                      'DE': 'greatdeepelf', 'Dg': 'greatdemigod', 'Ds': 'greatdemonspawn',
+                      'Dr': 'greatdraconian', 'Fe': 'greatfelid', 'Fo': 'greatformicid',
+                      'Gh': 'greatghoul', 'Gr': 'greatgargoyle', 'HE': 'greathighelf',
+                      'HO': 'greathillorc', 'Ha': 'greathalfling', 'Hu': 'greathuman',
+                      'Ko': 'greatkobold', 'Mf': 'greatmerfolk', 'Mi': 'greatminotaur',
+                      'Mu': 'greatmummy', 'Na': 'greatnaga', 'Op': 'greatoctopode',
+                      'Og': 'greatogre', 'Sp': 'greatspriggan', 'Te': 'greattengu',
+                      'Tr': 'greattroll', 'VS': 'greatvinestalker', 'Vp': 'greatvampire'}
+ROLE_TO_GREAT_ROLE = {'AE': 'greatairelementalist', 'AK': 'greatabyssalknight',
+                      'AM': 'greatarcanemarksman', 'Ar': 'greatartificer', 'As': 'greatassassin',
+                      'Be': 'greatberserker', 'CK': 'greatchaosknight', 'Cj': 'greatconjurer',
+                      'DK': 'greatdeathknight', 'EE': 'greatearthelementalist',
+                      'En': 'greatenchanter', 'FE': 'greatfireelementalist',
+                      'Fi': 'greatfighter', 'Gl': 'greatgladiator', 'He': 'greathealer',
+                      'Hu': 'greathunter', 'IE': 'greaticeelementalist', 'Mo': 'greatmonk',
+                      'Ne': 'greatnecromancer', 'Sk': 'greatskald', 'Su': 'greatsummoner',
+                      'Tm': 'greattransmuter', 'VM': 'greatvenommage', 'Wn': 'greatwanderer',
+                      'Wr': 'greatwarper', 'Wz': 'greatwizard'}
