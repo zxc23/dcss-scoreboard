7da4101631f5511610fc887aa8118122ddfdd6cb
Alex Jurkiewicz <alex@jurkiewi.cz>
2016-04-18 23:45:00 +1000

    Add some more logic to handle strange logfiles.
    * Handle fields without key & value
    * Handle non-ascii logfiles (korean & japanese)
    * Handle log entries without dates

--- scoreboard/log_import.py
+++ scoreboard/log_import.py
@@ -39,7 +39,7 @@ def load_logfile(logfile):
     processed_lines = model.get_log_pos(logfile)
     print("Reading %s%s... " % (logfile, (" from line %s" % processed_lines) if
                                 processed_lines else ''))
-    for line in open(logfile).readlines():
+    for line in open(logfile, encoding='utf-8').readlines():
         lines += 1
         # skip up to the first unprocessed line
         if lines <= processed_lines:
@@ -50,22 +50,33 @@ def load_logfile(logfile):
 
         log = {}
         log['src'] = src
+        badline = False
         for field in re.split(pat, line):
             # skip blank fields
             if not field:
                 continue
-            k, v = field.split('=', 1)
+            fields = field.split('=', 1)
+            if len(fields) != 2:
+                badline = True
+                print("Couldn't parse this line (bad field %s), skipping: %s" % (field, line))
+                continue
+            k, v = fields[0], fields[1]
             # Store numbers as int, not str
             try:
                 v = int(v)
             except ValueError:
                 v = v.replace("::", ":")  # Undo logfile escaping
             log[k] = v
+        if badline:
+            continue
         log['rc'] = log['char'][:2]
         log['bg'] = log['char'][2:]
         if 'god' not in log:
             log['god'] = 'Atheist'
         log['god'] = constants.GOD_NAME_FIXUPS.get(log['god'], log['god'])
+        if 'start' not in log:
+            print("Couldn't parse this line (missing start), skipping: %s" % line)
+            continue
         gid = calculate_game_gid(log)
         # Store the game in the database
         try:
