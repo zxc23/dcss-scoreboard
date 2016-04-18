201b63a8285ea306c3d6f93a5b3055abda95a79b
Alex Jurkiewicz <alex@jurkiewi.cz>
2016-04-18 23:45:00 +1000

    Pull read_logfile out of read_logfiles.

--- scoreboard/log_import.py
+++ scoreboard/log_import.py
@@ -3,6 +3,8 @@
 import os
 import re
 import time
+import glob
+import sys
 
 from . import model
 from . import constants
@@ -14,65 +16,67 @@ def calculate_game_gid(log):
 
 
 def load_logfiles():
-    """Read logfiles and parse their data.
+    """Read logfiles and parse their data."""
+    for logfile in glob.glob("logfiles/*"):
+        load_logfile(logfile)
 
-    XXX: should be given a filename to read, not figure it out ourselves.
-    """
-    import glob
+
+def load_logfile(logfile):
+    """Load a single logfile into the database."""
+    if os.stat(logfile).st_size == 0:
+        return
     # logfile format escapes : as ::, so we need to split with re.split
     # instead of naive line.split(':')
     pat = '(?<!:):(?!:)'
-    for logfile in glob.glob("logfiles/*"):
-        start = time.time()
-        # should be server source, eg cao, cpo, etc
-        src = os.path.basename(logfile.split('-', 1)[0])
-        if os.stat(logfile).st_size == 0:
+    start = time.time()
+    # should be server source, eg cao, cpo, etc
+    src = os.path.basename(logfile.split('-', 1)[0])
+    lines = 0
+    # How many lines have we already processed?
+    processed_lines = model.get_log_pos(logfile)
+    print("Reading %s%s... " % (logfile,
+                                (" from line %s" % processed_lines) if
+                                processed_lines else ''),
+          end='')
+    sys.stdout.flush()
+    for line in open(logfile).readlines():
+        lines += 1
+        # skip up to the first unprocessed line
+        if lines <= processed_lines:
             continue
-        lines = 0
-        # How many lines have we already processed?
-        processed_lines = model.get_log_pos(logfile)
-        print("Reading %s%s... " % (logfile,
-                                    (" from line %s" % processed_lines) if
-                                    processed_lines else ''),
-              end='')
-        for line in open(logfile).readlines():
-            lines += 1
-            # skip up to the first unprocessed line
-            if lines <= processed_lines:
-                continue
-            # skip blank lines
-            if not line:
-                continue
+        # skip blank lines
+        if not line:
+            continue
 
-            log = {}
-            log['src'] = src
-            for field in re.split(pat, line):
-                # skip blank fields
-                if not field:
-                    continue
-                k, v = field.split('=', 1)
-                # Store numbers as int, not str
-                try:
-                    v = int(v)
-                except ValueError:
-                    v = v.replace("::", ":")  # Undo logfile escaping
-                log[k] = v
-            log['rc'] = log['char'][:2]
-            log['bg'] = log['char'][2:]
-            if 'god' not in log:
-                log['god'] = 'Atheist'
-            log['god'] = constants.GOD_NAME_FIXUPS.get(log['god'], log['god'])
-            gid = calculate_game_gid(log)
-            # Store the game in the database
+        log = {}
+        log['src'] = src
+        for field in re.split(pat, line):
+            # skip blank fields
+            if not field:
+                continue
+            k, v = field.split('=', 1)
+            # Store numbers as int, not str
             try:
-                model.add_game(gid, log)
-            except model.DatabaseError as e:
-                print(e)
-        # Save the new number of lines processed in the database
-        model.save_log_pos(logfile, lines)
-        end = time.time()
-        print("done (%s new lines) in %s secs" % (lines - processed_lines,
-                                                  round(end - start, 2)))
+                v = int(v)
+            except ValueError:
+                v = v.replace("::", ":")  # Undo logfile escaping
+            log[k] = v
+        log['rc'] = log['char'][:2]
+        log['bg'] = log['char'][2:]
+        if 'god' not in log:
+            log['god'] = 'Atheist'
+        log['god'] = constants.GOD_NAME_FIXUPS.get(log['god'], log['god'])
+        gid = calculate_game_gid(log)
+        # Store the game in the database
+        try:
+            model.add_game(gid, log)
+        except model.DatabaseError as e:
+            print(e)
+    # Save the new number of lines processed in the database
+    model.save_log_pos(logfile, lines)
+    end = time.time()
+    print("done (%s new lines) in %s secs" % (lines - processed_lines,
+                                              round(end - start, 2)))
 
 
 if __name__ == "__main__":
