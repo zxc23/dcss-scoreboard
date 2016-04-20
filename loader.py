9d5e898cd38ba86fce07c3ba6dbb4627689a2283
Alex Jurkiewicz <alex@jurkiewi.cz>
2016-04-20 14:39:38 +1000

    Make default loader behaviour not to rebuild scoring.

--- loader.py
+++ loader.py
@@ -7,5 +7,5 @@ import scoreboard.write_website
 scoreboard.log_import.load_logfiles()
 #scoreboard.scoring.rescore_player('zzxc')
 #scoreboard.scoring.score_games()
-scoreboard.scoring.score_games(rebuild=True)
+scoreboard.scoring.score_games()
 scoreboard.write_website.write_website()
