ab1248d50c0bb66fea664a7dccfb941591db59c2
Alex Jurkiewicz <alex@jurkiewi.cz>
2016-07-13 21:10:24 +1000

    Update Vagrantfile.
    Not 100% working yet.

--- setup1.sh
+++
@@ -1,4 +0,0 @@
-curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
-echo 'export PATH="/home/vagrant/.pyenv/bin:$PATH"' >>~/.bash_profile
-echo 'eval "$(pyenv init -)"' >>~/.bash_profile
-echo 'eval "$(pyenv virtualenv-init -)"' >>~/.bash_profile
--- setup2.sh
+++
@@ -1,3 +0,0 @@
-pyenv install 3.5.1
-pyenv global 3.5.1
-pip install -r /vagrant/requirements.txt
