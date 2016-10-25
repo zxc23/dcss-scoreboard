6a378b8f20a8cb47497093ad77174518051e7e2d
David Kerksal <dkerksal@gmail.com>
2016-10-26 02:53:16 +1100

    Add a bot to the list

--- scoreboard/constants.py
+++ scoreboard/constants.py
@@ -214,7 +214,7 @@ BLACKLISTS = {
         'notqw', 'jw', 'parabodrick', 'hyperqwbe', 'cashybrid', 'tstbtto',
         'parabolic', 'oppbolic', 'ew', 'rushxxi', 'gaubot', 'cojitobot',
         'paulcdejean', 'otabotab', 'nakatomy', 'testingqw', 'beemell', 'beem',
-        'drasked', 'phybot', 'khrogbot'
+        'drasked', 'phybot', 'khrogbot', 'jwbot'
     },
     'bot-games': {'LoveLain:cao:20160903064250S'}
 }
