c0ea3268d2083006aa29319aef1b9b268b07f024
David Kerksal <dkerksal@gmail.com>
2016-12-06 01:41:00 +1100

    Create a basic winning games table
    Note that dates and sorting by dates are both messed up.

--- vue-prototype/src/components/GamesTable.vue
+++ vue-prototype/src/components/GamesTable.vue
@@ -2,9 +2,7 @@
 </style>
 
 <template>
-  <div>
-    games table here
-  </div>
+  <v-client-table :data="games" :columns="columns" :options="options"></v-client-table>
 </template>
 
 <script>
@@ -14,6 +12,39 @@
     props: ['games'],
     data: function () {
       return {
+        options: {
+          filterable: false,
+          headings: {
+            char: 'Combo',
+            tmsg: 'End',
+            dur: 'Duration'
+          },
+          perPage: 20,
+          perPageValues: [],
+          orderBy: {
+            column: 'end'
+          },
+          templates: {
+            dur: function (h, row) {
+              let dur = row.dur
+              let hh = (~~(dur / 3600)).toString()
+              while (hh.length < 2) hh = '0' + hh
+              let mm = (~~(dur % 3600 / 60)).toString()
+              while (mm.length < 2) mm = '0' + mm
+              let ss = (dur % 60).toString()
+              while (ss.length < 2) ss = '0' + ss
+              return `${hh}:${mm}:${ss}`
+            },
+            date: function (h, row) {
+              return new Date(Number(row.end) * 1000).toLocaleString()
+            }
+          }
+        }
+      }
+    },
+    computed: {
+      columns: () => {
+        return ['score', 'char', 'god', 'runes', 'turns', 'dur', 'date', 'version']
       }
     }
   }
