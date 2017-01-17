f9a05f745582825ecc3a68638aea29ad29851e9a
Alex Jurkiewicz <alex@jurkiewi.cz>
2017-01-17 11:07:03 +1100

    Add Dead Man's Snitch
    Alluded to in the previous commit, this is tracking to make sure the
    scoreboard is running properly in production.
    
    We don't check in the snitch script since the URL is secret.

--- contrib/run.sh
+++ contrib/run.sh
@@ -5,4 +5,4 @@
 
 cd /app/scoreboard
 . /root/venv/bin/activate
-./loader.py --urlbase 'https://scoreboard.crawl.develz.org' --extra-player-pages 50
+./loader.py --urlbase 'https://scoreboard.crawl.develz.org' --extra-player-pages 50 && ./update-dms.sh
